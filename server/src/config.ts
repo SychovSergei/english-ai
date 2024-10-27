c04f06e416d9722cd321d9848b53c78bfc2801bd
SychovSergei <sychov.sergei@gmail.com>
2024-10-27 21:29:53 +0200

    [chore(lint)]: format files with prettier config

--- server/src/config.ts
+++ server/src/config.ts
@@ -7,13 +7,13 @@ dotenv.config({ path: envPath });
 const { PORT, OPENAI_API_KEY, DATABASE_SOURCE } = process.env;
 
 const conf = {
-    port: PORT,
-    mongo: {
-        db_source: DATABASE_SOURCE,
-    },
-    openAi: {
-        api_key: OPENAI_API_KEY,
-    }
+  port: PORT,
+  mongo: {
+    db_source: DATABASE_SOURCE,
+  },
+  openAi: {
+    api_key: OPENAI_API_KEY,
+  },
 };
 
 export default conf;
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,25 +4,24 @@ import app from "./bin/app";
 import config from "./config";
 
 const dbConnect = async () => {
-    try {
-        const dbSource = config.mongo.db_source!;
-        await mongoose.connect(dbSource);
-        console.log("Connected to MongoDB success");
-    } catch (e) {
-        console.log("Error: connection to DB failed");
-    }
+  try {
+    const dbSource = config.mongo.db_source!;
+    await mongoose.connect(dbSource);
+    console.log("Connected to MongoDB success");
+  } catch (e) {
+    console.log("Error: connection to DB failed");
+  }
 };
 
 const start = async () => {
-    try {
-        await dbConnect();
+  try {
+    await dbConnect();
 
-        const PORT = config.port;
-        app.listen(PORT, () => console.log(`[server]: Server is running at PORT:${PORT}`));
-    }
-    catch (e) {
-        console.log("Start() ERROR, e");
-    }
+    const PORT = config.port;
+    app.listen(PORT, () => console.log(`[server]: Server is running at PORT:${PORT}`));
+  } catch (e) {
+    console.log("Start() ERROR, e");
+  }
 };
 
 start().catch((err) => console.log("err", err));
