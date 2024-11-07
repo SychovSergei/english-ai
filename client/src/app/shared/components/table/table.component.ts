a8783b9a3081643825a403fab5c338aa1dd28067
SychovSergei <sychov.sergei@gmail.com>
2024-11-08 01:08:26 +0200

    chore(table): apply eslint and prettier rules for table component

--- client/src/app/shared/components/table/table.component.ts
+++ client/src/app/shared/components/table/table.component.ts
@@ -1,27 +1,13 @@
-import {
-  Component,
-  effect,
-  Input,
-  OnInit,
-  signal,
-  TemplateRef,
-  ViewChild,
-  WritableSignal
-} from '@angular/core';
-
-import { SelectionModel } from '@angular/cdk/collections';
-import { MatPaginator } from '@angular/material/paginator';
-import {
-
-  MatTableDataSource
-} from '@angular/material/table';
-import { MatCheckbox, MatCheckboxChange } from '@angular/material/checkbox';
-
-import { MatTableModule } from '@angular/material/table';
-import { MatIcon } from '@angular/material/icon';
-import { MatIconButton } from '@angular/material/button';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import { SelectionModel } from '@angular/cdk/collections';
 import { JsonPipe, NgIf, NgTemplateOutlet } from '@angular/common';
+import { Component, effect, Input, OnInit, signal, TemplateRef, ViewChild, WritableSignal } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatCheckbox, MatCheckboxChange } from '@angular/material/checkbox';
+import { MatIcon } from '@angular/material/icon';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 
 interface Identifiable {
   id: string | number;
@@ -30,115 +16,100 @@ interface Identifiable {
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [
-
-    JsonPipe,
-
-    MatTableModule,
-    MatCheckbox,
-    MatIcon,
-    MatIconButton,
-    MatPaginator,
-    NgTemplateOutlet,
-    NgIf
-  ],
+  imports: [JsonPipe, MatTableModule, MatCheckbox, MatIcon, MatIconButton, MatPaginator, NgTemplateOutlet, NgIf],
   animations: [
     trigger('detailExpand', [
-      state('collapsed,void', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
+      state('collapsed,void', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrl: './table.component.scss',
 })
 export class TableComponent<T extends Identifiable> implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
 
-  private _columnInit: string[] = ["select"];
-  columnsAdditional: string[] = ['originalText',
-    'language',
-    'translateValues',
-    'voice',];
+  private _columnInit: string[] = ['select'];
+  columnsAdditional: string[] = ['originalText', 'language', 'translateValues', 'voice'];
 
-  columns: WritableSignal<string[]> = signal(["select"]);
+  columns: WritableSignal<string[]> = signal(['select']);
   @Input()
   set columnsInitial(val: string[]) {
-    console.log("ИЗМЕНИЛИСЬ columns");
-    this.columns.set([...this._columnInit, ...val])// = [ ...this._columnInit, ...val];
+    console.log('ИЗМЕНИЛИСЬ columns');
+    this.columns.set([...this._columnInit, ...val]); // = [ ...this._columnInit, ...val];
   }
 
-  @Input() dataSource!: MatTableDataSource<T>;// = new MatTableDataSource<T>([]);
+  @Input() dataSource!: MatTableDataSource<T>; // = new MatTableDataSource<T>([]);
   // @Input() selection!: SelectionModel<T>;// = new SelectionModel<T>(true, []);
 
-  @Input() expandTemplate!: TemplateRef<any>;// = new MatTableDataSource<T>([]);
+  @Input() expandTemplate!: TemplateRef<unknown>; // = new MatTableDataSource<T>([]);
 
   isShowSelection: WritableSignal<boolean> = signal(true);
   @Input()
   set showSelection(val: boolean) {
-    console.log("ИЗМЕНИЛИСЬ isShowSelection");
+    console.log('ИЗМЕНИЛИСЬ isShowSelection');
     this.isShowSelection.set(val);
-    console.log("SHO W  W  W",this.isShowSelection());
+    console.log('SHO W  W  W', this.isShowSelection());
   }
 
-  templates: { [key: string]: TemplateRef<any> } = {};
+  templates: { [key: string]: TemplateRef<unknown> } = {};
   keys = Object.keys(this.templates);
   @Input()
-  set ceilTemplates(data: { [key: string]: TemplateRef<any> }) {
-    console.log("ИЗМЕНИЛИСЬ шаблоны");
+  set ceilTemplates(data: { [key: string]: TemplateRef<unknown> }) {
+    console.log('ИЗМЕНИЛИСЬ шаблоны');
     this.templates = data;
     this.keys = Object.keys(this.templates);
-  };
-
-
-  selection!: WritableSignal<SelectionModel<T>>;// = signal(new SelectionModel<IWordTableData>(true, []));
-  @Input()
-  set selectionInput(sel: WritableSignal<SelectionModel<T>>) {
-    console.log("ИЗМЕНИЛИСЬ selection");
-    this.selection = sel;//.set(sel);
   }
 
-  private _data!: WritableSignal<T[]>;// = signal([]);
+  selection!: WritableSignal<SelectionModel<T>>; // = signal(new SelectionModel<IWordTableData>(true, []));
+  @Input()
+  set selectionInput(sel: WritableSignal<SelectionModel<T>>) {
+    console.log('ИЗМЕНИЛИСЬ selection');
+    this.selection = sel; //.set(sel);
+  }
+
+  private _data!: WritableSignal<T[]>; // = signal([]);
   @Input()
   set data(value: WritableSignal<T[]>) {
-    console.log("ИЗМЕНИЛИСЬ ДАННЫЕ", value());
-    this._data = value//.set(value);
+    console.log('ИЗМЕНИЛИСЬ ДАННЫЕ', value());
+    this._data = value; //.set(value);
   }
 
   constructor() {
-    console.log("columns =", this.columns());
+    console.log('columns =', this.columns());
     effect(() => {
-      console.log("=====EFFECT==data===");
-      console.log("data", this._data());
+      console.log('=====EFFECT==data===');
+      console.log('data', this._data());
       this.dataSource.data = this._data();
       this.dataSource.paginator = this.paginator;
     });
     effect(() => {
-      console.log("=====EFFECT==selection===");
-      console.log("selection====", this.selection());
+      console.log('=====EFFECT==selection===');
+      console.log('selection====', this.selection());
     });
     effect(() => {
-      console.log("=====EFFECT==columns===");
-      console.log("columns columns columns ====", this.columns());
+      console.log('=====EFFECT==columns===');
+      console.log('columns columns columns ====', this.columns());
     });
     effect(() => {
-      console.log("=====EFFECT==columnsToDisplayWithExpand===");
-      console.log("columnsToDisplayWithExpand ====", this.columnsToDisplayWithExpand());
+      console.log('=====EFFECT==columnsToDisplayWithExpand===');
+      console.log('columnsToDisplayWithExpand ====', this.columnsToDisplayWithExpand());
     });
   }
 
-  columnsToDisplayWithExpand:  WritableSignal<string[]> = signal(['select',...this.columns(), 'expand','actions']);
+  columnsToDisplayWithExpand: WritableSignal<string[]> = signal(['select', ...this.columns(), 'expand', 'actions']);
   @Input()
   set columnsToDisplayWithExpandInput(val: string[]) {
-    this.columnsToDisplayWithExpand.set(['select', ...val, 'expand','actions']);
+    this.columnsToDisplayWithExpand.set(['select', ...val, 'expand', 'actions']);
     console.log(this.columnsToDisplayWithExpand());
   }
 
   @Input() expandedElement!: T | null;
 
   ngOnInit() {
-    console.log(">> ngOnInitt his.columns", this.columns());
-    console.log(">> ngOnInit templates >>> ",this.templates);
+    console.log('>> ngOnInitt his.columns', this.columns());
+    console.log('>> ngOnInit templates >>> ', this.templates);
   }
 
   /** Whether the number of selected elements matches the total number of rows. */
@@ -170,8 +141,8 @@ export class TableComponent<T extends Identifiable> implements OnInit {
     console.log(event);
     console.log(row);
 
-    this.selection.update(selection => {
-      console.log("UPDATE");
+    this.selection.update((selection) => {
+      console.log('UPDATE');
       selection.toggle(row);
       return selection; // Возвращаем обновлённый объект
     });
@@ -179,5 +150,4 @@ export class TableComponent<T extends Identifiable> implements OnInit {
 
     // event.checked ? this.selection().select(row) : this.selection().deselect(row);
   }
-
 }
