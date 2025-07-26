9a161daf5ff06241a3e7a23e5dd8527fa7c4dea1
SychovSergei <sychov.sergei@gmail.com>
2025-07-26 21:28:49 +0300

    client: chore: add TODO mark for check in the future

--- client/src/app/widgets/table-widget/table-widget.module.ts
+++ client/src/app/widgets/table-widget/table-widget.module.ts
@@ -1,28 +1,12 @@
-import { TableActionsModule } from '@shared/ui/data-table-actions';
+import { UiKitModule } from '@shared/ui/ui-kit';
 
 import { NgModule } from '@angular/core';
-import { MatButton } from '@angular/material/button';
-import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatIcon } from '@angular/material/icon';
-import { MatInput } from '@angular/material/input';
 
 import { TableWidgetComponent } from './table-widget.component';
 
 @NgModule({
   exports: [TableWidgetComponent],
   declarations: [TableWidgetComponent],
-  imports: [
-    MatButton,
-    MatCard,
-    MatCardContent,
-    MatCardHeader,
-    MatFormField,
-    MatIcon,
-    MatInput,
-    MatLabel,
-
-    TableActionsModule,
-  ],
+  imports: [UiKitModule],
 })
 export class TableWidgetModule {}
