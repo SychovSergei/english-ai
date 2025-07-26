ab67213ac9b1bb37bdb091eb19a9ee8cf07ea499
SychovSergei <sychov.sergei@gmail.com>
2025-07-26 23:34:46 +0300

    client: refactor(pages/words): move unnecessary lines

--- client/src/app/pages/words/words.page.module.ts
+++ client/src/app/pages/words/words.page.module.ts
@@ -1,29 +1,15 @@
-// import { TableActionsModule } from '@features/table-actions';
-// import { WordTableActionsComponent, WordTableComponent } from '@features/words';
-
-import { WordService } from '@entities/word';
-import { WORD_SERVICE_TOKEN } from '@features/words/model/word.tokens';
+import { WordService } from '@features/words';
+import { WORD_SERVICE_TOKEN } from '@features/words';
 import { NotificationService } from '@shared/services/notification/notification.service';
 import { NOTIFICATION_SERVICE_TOKEN } from '@shared/services/notification/notification-service.token';
-// import { TableWidgetModule } from '@widgets/table-widget';
 import { WordManagementComponent } from '@widgets/word-management/ui/word-management.component';
 
 import { NgModule } from '@angular/core';
 
-// import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
 import { WordsPage } from './words.page';
 
 @NgModule({
-  imports: [
-    // TableWidgetModule,
-    // TableActionsModule,
-    // MatCard,
-    // MatCardContent,
-    // MatCardHeader,
-    // WordTableComponent,
-    // WordTableActionsComponent,
-    WordManagementComponent,
-  ],
+  imports: [WordManagementComponent],
   declarations: [WordsPage],
   exports: [WordsPage],
   providers: [
