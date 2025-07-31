9db959a7dabc30da0191102133f546880edcffd0
SychovSergei <sychov.sergei@gmail.com>
2025-07-31 23:17:28 +0300

    server: feat: add ClientMeta to the auth methods for saving the Origin and Agent information

--- server/src/infrastructure/db/repositories/token-repository.service.ts
+++ server/src/infrastructure/db/repositories/token-repository.service.ts
@@ -2,12 +2,13 @@ import { injectable } from 'inversify';
 
 import { UserRefreshToken } from '@core/domain/entities';
 import { ITokenRepositoryService } from '@core/repositories';
+import { ClientMeta } from '@core/repositories/auth-repository/auth.service.interface';
 import { TokenModel } from '@infrastructure/db/entities';
 
 @injectable()
 export class TokenRepositoryService implements ITokenRepositoryService {
-  async findByUserId(userId: string): Promise<UserRefreshToken | null> {
-    const token = await TokenModel.findOne({ userId });
+  async findByUserId(userId: string, meta: ClientMeta): Promise<UserRefreshToken | null> {
+    const token = await TokenModel.findOne({ userId, origin: meta.origin, userAgent: meta.userAgent });
     return token !== null ? token.toObject() : null;
   }
 
@@ -19,8 +20,8 @@ export class TokenRepositoryService implements ITokenRepositoryService {
     );
   }
 
-  async createToken(userId: string, refreshToken: string): Promise<UserRefreshToken> {
-    const newToken = await TokenModel.create({ userId, refreshToken });
+  async createToken(userId: string, refreshToken: string, meta: ClientMeta): Promise<UserRefreshToken> {
+    const newToken = await TokenModel.create({ userId, refreshToken, origin: meta.origin, userAgent: meta.userAgent });
     return newToken.toObject();
   }
 
