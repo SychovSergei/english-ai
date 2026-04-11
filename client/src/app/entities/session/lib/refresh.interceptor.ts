302c5846de350da50761403aa41aa73ef5f243c1
SychovSergei <sychov.sergei@gmail.com>
2026-04-11 13:28:17 +0300

    refactor(session): cleanup refreshInterceptor dependencies and logging

--- client/src/app/entities/session/lib/refresh.interceptor.ts
+++ client/src/app/entities/session/lib/refresh.interceptor.ts
@@ -1,10 +1,10 @@
-import { AuthService } from '@entities/session/api/auth.service';
+import { AuthService } from '@entities/session';
+import { LoggerService } from '@shared/lib/logger/logger.service';
 
 import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Observable, switchMap, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { LoggerService } from '@shared/lib/logger/logger.service';
 
 /**
  * Интерсептор для обновления токена аутентификации.
@@ -18,25 +18,20 @@ export const refreshInterceptor: HttpInterceptorFn = (
 ): Observable<HttpEvent<unknown>> => {
   // Извлекаем сервис аутентификации для обновления токена.
   const authService = inject(AuthService);
-  const loggerService = inject(LoggerService);
+  const loggerService = inject(LoggerService).createLogger('refreshInterceptor');
 
-  // Логируем URL запроса для отладки.
-  loggerService.log('[refreshInterceptor] Request URL:', req.url.toString());
-
-  // Передаем запрос дальше по цепочке интерсепторов.
-  loggerService.log('[refreshInterceptor] URL', req.url.toString());
   // Передаём запрос дальше по цепочке интерсепторов
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
       // Проверяем, что это 401 и это НЕ запрос на сам refresh
-      loggerService.log('[refreshInterceptor]: error.status =====', error.status);
-      loggerService.log('?????[refreshInterceptor]: req.url =====', req.url);
+      loggerService.log('error.status =====', error.status);
+      loggerService.log('req.url =====', req.url);
       if (error.status === 401 && !req.url.includes('auth/refresh')) {
         //&& !req.url.includes('auth/login') && !req.url.includes('auth/refresh')
-        loggerService.warn('?????[refreshInterceptor]: Refresh token process ./././');
+        loggerService.warn('Refresh token process ./././');
         return authService.refreshTokens().pipe(
           switchMap((newToken) => {
-            loggerService.log('[refreshInterceptor]: Refresh NEW token', newToken);
+            loggerService.log('Refresh NEW token', newToken);
             // Repeat the request with the new access token
             const retryReq = req.clone({
               setHeaders: { Authorization: `Bearer ${newToken}` },
