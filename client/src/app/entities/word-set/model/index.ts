f45e05f7c928c25901872e4b6b801240c173194c
SychovSergei <sychov.sergei@gmail.com>
2026-04-29 19:10:59 +0300

    EAD-1: refactor(word-set): implement WordSet entity and domain logic

---
+++ client/src/app/entities/word-set/model/index.ts
@@ -0,0 +1,5 @@
+export { WordSetId } from './vo/word-set-id.vo';
+export type { WordSetSettingsProps } from './vo/word-set-settings.vo';
+export { WordSetSettings } from './vo/word-set-settings.vo';
+export { EWordSetVisibility } from './vo/word-set-visibility.enum';
+export type { WordSetProps } from './word-set.types';
